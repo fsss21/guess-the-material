61f4a646bd62aa82792d376a757e9976e2698403
Ruslan <rnagumanov57@gmail.com>
2026-01-17 17:05:54 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Game from './components/Game'
+import styles from './App.module.css'
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <Game />
+    </div>
+  )
+}
+
+export default App
